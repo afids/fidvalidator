698ee81f64630a141c5777ecf581c9eed69154ea
Jason Kai <tkai@uwo.ca>
2021-05-18 15:32:00 -0400

    MAINT: rm unnecessary env call in manage.py

--- manage.py
+++ manage.py
@@ -7,18 +7,6 @@ from config import DevelopmentConfig
 
 from controller import app, db
 
-if os.environ.get("FLASK_ENV").lower() == "production":
-    from config import ProductionConfig
-
-    config_settings = ProductionConfig
-elif os.environ.get("FLASK_ENV").lower() == "testing":
-    from config import TestingConfig
-
-    config_settings = TestingConfig
-else:
-    config_settings = DevelopmentConfig
-app.config.from_object(config_settings)
-
 migrate = Migrate(app, db)
 manager = Manager(app)
 
